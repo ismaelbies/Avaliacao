3e6fb317618c5c88da6cfd9d41c937328f2ed03b
Jhonatan Mariani <jhonatan_mariani@edu.univali.br>
2022-06-25 23:57:48 -0300

    Add professor cadastro

--- routes/system/cadastros.php
+++ routes/system/cadastros.php
@@ -4,3 +4,4 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/cadastros/aluno/', fn(Request $request, Response $response) => $this->AlunoController->index($request, $response));
+$app->get('/cadastros/professor/', fn(Request $request, Response $response) => $this->ProfessorController->index($request, $response));
--- src/Controllers/ProfessorController.php
+++ src/Controllers/ProfessorController.php
@@ -4,7 +4,11 @@
 namespace App\Controllers;
 
 
+use Slim\Http\Request;
+use Slim\Http\Response;
 class ProfessorController extends Controller
 {
-
+    public function index(Request $request, Response $response) {
+        return $this->renderer->render($response, 'default.phtml', ['page' => 'cadastros/professor.phtml']);
+    }
 }
