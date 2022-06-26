0cdb53f7ab3f07028dfca9a47c43331ae942038c
isma <ismaelbies@hotmail.com>
2022-06-26 13:20:35 -0300

    cadastros professor

--- routes/api/cadastros.php
+++ routes/api/cadastros.php
@@ -6,3 +6,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 $app->post('/api/cadastros/aluno/', fn(Request $request, Response $response) => $this->AlunoController->registerAluno($request, $response));
 
 $app->get('/api/cadastros/aluno/', fn(Request $request, Response $response) => $this->AlunoController->getAlunos($request, $response));
+
+$app->post('/api/cadastros/professor/', fn(Request $request, Response $response) => $this->ProfessorController->registerProfessor($request, $response));
+
+$app->get('/api/cadastros/professor/', fn(Request $request, Response $response) => $this->ProfessorController->getProfessores($request, $response));
--- src/Models/Entities/Professor.php
+++ src/Models/Entities/Professor.php
@@ -3,94 +3,39 @@
 
 namespace App\Models\Entities;
 
-
+/**
+ * @Entity @Table(name="professores")
+ * @ORM @Entity(repositoryClass="App\Models\Repository\ProfessorRepository")
+ */
 class Professor extends UserQuiz
 {
-    //public string $matricula = '';
-
-    public function __construct(string $name, string $email, string $password)
-    {
-        parent::__construct($name, $email, $password);
-    }
-
-    /*public function getMatricula(): string
-    {
-        return $this->matricula;
-    }*/
-
-    /*public function setMatricula(string $matricula): Aluno
-    {
-        $this->matricula = $matricula;
-        return $this;
-    }*/
-
-    /**
-     * @return int|null
-     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @param int|null $id
-     * @return Aluno
-     */
-    public function setId(?int $id): Aluno
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param string $name
-     * @return Aluno
-     */
-    public function setName(string $name): Aluno
+    public function setName(string $name): Professor
     {
         $this->name = $name;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * @param string $email
-     * @return Aluno
-     */
-    public function setEmail(string $email): Aluno
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
     public function getPassword(): string
     {
         return $this->password;
     }
 
-    /**
-     * @param string $password
-     * @return Aluno
-     */
-    public function setPassword(string $password): Aluno
+    public function setPassword(string $password): Professor
     {
         $this->password = $password;
         return $this;
